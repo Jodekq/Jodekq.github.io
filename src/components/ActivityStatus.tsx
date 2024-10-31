1846104171f58364d347716199b89eaf6d283951
Jonas <60574217+Jodekq@users.noreply.github.com>
2024-10-31 11:42:01 +0100

    improved responsive

--- src/components/ActivityStatus.tsx
+++ src/components/ActivityStatus.tsx
@@ -80,33 +80,35 @@ const ActivityStatus: React.FC = () => {
    );
 
    return (
-      <div className="flex flex-row flex-wrap items-center text-emerald-50 gap-5">
-         <a href="https://discord.com/users/Jodek" target="_blank" className="flex flex-row items-center border border-emerald-200 rounded-lg justify-between px-5 pb-1 h-20 font-sans text-xl text-emerald-50 gap-3">
-            <i className="bx bxl-discord-alt text-4xl"></i>
-            <div>
-               <p className="text-lg font-semibold">
-                  @{data.discord_user.username}
-               </p>
-               <p className="flex items-center text-sm">
-                   <div
-                       className={`w-2.5 h-2.5 rounded-full mr-2 ${
-                           data.discord_status === 'online' ? 'bg-green-500 animate-pulse' :
-                           data.discord_status === 'dnd' ? 'bg-red-500' :
-                           data.discord_status === 'idle' ? 'bg-yellow-500' : 'bg-gray-500'
-                       }`}
-                   ></div>
-                   {data.activities && data.activities.length > 0 ? (
-                       `${data.activities[0].name}`
-                   ) : (
-                       data.discord_status === 'online' ? 'Online' :
-                       data.discord_status === 'dnd' ? 'Do Not Disturb' :
-                       data.discord_status === 'idle' ? 'Idle' : 'Offline'
-                   )}
-               </p>
+      <div className="flex flex-row grow flex-wrap items-center text-emerald-50 gap-5">
+         <a href="https://discord.com/users/Jodek" target="_blank" className="flex flex-row grow items-center border border-emerald-200 rounded-lg justify-between px-5 pb-1 h-20 font-sans text-xl text-emerald-50 gap-3">
+            <div className='flex flex-row gap-3 items-center'>
+               <i className="bx bxl-discord-alt text-4xl"></i>
+               <div>
+                  <p className="text-lg font-semibold">
+                     @{data.discord_user.username}
+                  </p>
+                  <p className="flex items-center text-sm">
+                      <div
+                          className={`w-2.5 h-2.5 rounded-full mr-2 ${
+                              data.discord_status === 'online' ? 'bg-green-500 animate-pulse' :
+                              data.discord_status === 'dnd' ? 'bg-red-500' :
+                              data.discord_status === 'idle' ? 'bg-yellow-500' : 'bg-gray-500'
+                          }`}
+                      ></div>
+                      {data.activities && data.activities.length > 0 ? (
+                          `${data.activities[0].name}`
+                      ) : (
+                          data.discord_status === 'online' ? 'Online' :
+                          data.discord_status === 'dnd' ? 'Do Not Disturb' :
+                          data.discord_status === 'idle' ? 'Idle' : 'Offline'
+                      )}
+                  </p>
+               </div>
             </div>
             <i className="bx bx-link-external cursor-pointer p-3 text-gray-400"></i>
          </a>
-         <div className="px-5 pb-1 border border-emerald-200 rounded-lg font-sans h-20 items-center flex flex-row gap-3">
+         <div className="px-5 pb-1 border border-emerald-200 rounded-lg font-sans h-20 items-center flex flex-row grow gap-3">
             <i className="bx bxl-spotify text-4xl"></i>
             <div className="flex-1">
                <p className="text-lg font-semibold">Listening to:</p>
